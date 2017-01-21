230d9938831883c6047191dd6faa29782439ab42
teej107 <teej107@gmail.com>
2017-01-21 11:17:40 -0700

    Done!

--- closures.js
+++ closures.js
@@ -1,236 +1,238 @@
 /******************************************************************************\
-	#PROBLEM-01
-\******************************************************************************/
+ #PROBLEM-01
+ \******************************************************************************/
 
-var outer = function(){
-  var name = 'Tyler';
-  return function(){
-    return 'The original name was ' + name;
-  }
+var outer = function ()
+{
+    var name = 'Tyler';
+    return function ()
+    {
+        return 'The original name was ' + name;
+    }
 };
 
 /****** INSTRUCTIONS PROBLEM 1 ******/
 /* Above you're given a function that returns another function which has a
-closure over the name variable. Invoke outer saving the return value into
-another variable called 'inner'. */
+ closure over the name variable. Invoke outer saving the return value into
+ another variable called 'inner'. */
 
 // Code Here
-
+var inner = outer();
 //Once you do that, invoke inner.
-
+console.log(inner());
 //Code Here
 
 
-
-
-
-
-
-
-
-
 /******************************************************************************\
-	#PROBLEM-02
-\******************************************************************************/
+ #PROBLEM-02
+ \******************************************************************************/
 
 
-var callFriend = function(){
-  var friend = 'Jake';
-  function callF(number){
-    return 'Calling ' + friend + ' at ' + number;
-  }
-  return callF;
+var callFriend = function ()
+{
+    var friend = 'Jake';
+
+    function callF(number)
+    {
+        return 'Calling ' + friend + ' at ' + number;
+    }
+
+    return callF;
 };
 
 /****** INSTRUCTIONS PROBLEM 2 ******/
 /* Above you're given a callFriend function that returns another function.
-Create a makeCall function that when invoked logs 'Calling Jake at 435-215-9248'
-in your console. */
+ Create a makeCall function that when invoked logs 'Calling Jake at 435-215-9248'
+ in your console. */
 
-  //Code Here
-
-
-
-
-
-
-
-
-
+//Code Here
+function makeCall()
+{
+    console.log(callFriend()("435-215-9248"));
+}
+makeCall();
 
 /******************************************************************************\
-	#PROBLEM-03
-\******************************************************************************/
+ #PROBLEM-03
+ \******************************************************************************/
 
 /****** INSTRUCTIONS PROBLEM 3 ******/
 /* Write a function called makeCounter that makes the following code work
-properly. */
+ properly. */
 
 //Code Here
-
+function makeCounter()
+{
+    var num = 0;
+    return function ()
+    {
+        console.log("count:", ++num);
+        return num;
+    };
+}
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
-
-
-
-
-
-
-
-
+var count = makeCounter();
+count(); // 1
+count(); // 2
+count(); // 3
+count(); // 4
 
 
 /******************************************************************************\
-	#PROBLEM-04
-\******************************************************************************/
+ #PROBLEM-04
+ \******************************************************************************/
 
 /****** INSTRUCTIONS PROBLEM 4 ******/
 /* Inside the function called counterFactory return two functions that implement
-up/down counter. The first function is called inc, this function is responsible
-for incrementing the value once. The second function is called dec, this
-function is responsible for decrementing the value by one. You will need to use
-the module pattern to achieve this. */
+ up/down counter. The first function is called inc, this function is responsible
+ for incrementing the value once. The second function is called dec, this
+ function is responsible for decrementing the value by one. You will need to use
+ the module pattern to achieve this. */
 
-function counterFactory(value) {
+function counterFactory(value)
+{
 
-  // Code here.
+    // Code here.
 
-
-  return {
-  }
+    return {
+        inc: function ()
+        {
+            return ++value;
+        },
+        dec: function ()
+        {
+            return --value;
+        }
+    }
 }
 
 
 counter = counterFactory(10);
-
-
-
-
-
-
-
-
-
+console.log("Counter Factory:", counter.inc());
+console.log("Counter Factory:", counter.inc());
+console.log("Counter Factory:", counter.dec());
 
 /******************************************************************************\
-	#PROBLEM-05
-\******************************************************************************/
+ #PROBLEM-05
+ \******************************************************************************/
 
 /****** INSTRUCTIONS PROBLEM 5 ******/
 /* Inside the motivation function create another function called message that
-will return 'You're doing awesome, keep it up firstname lastname.' */
+ will return 'You're doing awesome, keep it up firstname lastname.' */
 
-function motivation(firstname, lastname){
+function motivation(firstname, lastname)
+{
 
-  var welcomeText = 'You\'re doing awesome, keep it up ';
+    var welcomeText = 'You\'re doing awesome, keep it up ';
 
-  // code message function here.
+    // code message function here.
+    var message = function ()
+    {
+        return welcomeText + firstname + ' ' + lastname + ".";
+    };
 
-
-  //Uncommment this to return the value of your invoked message function
-  //return message();
+    //Uncommment this to return the value of your invoked message function
+    return message();
 
 }
 
-motivation('Billy', 'Bob'); // 'Your doing awesome keep it up Billy Bob
-
-
-
-
-
-
-
-
+console.log(motivation('Billy', 'Bob')); // 'Your doing awesome keep it up Billy Bob
 
 
 /******************************************************************************\
-	#PROBLEM-06
-\******************************************************************************/
+ #PROBLEM-06
+ \******************************************************************************/
 
 /****** INSTRUCTIONS PROBLEM 6 ******/
 /* Inside the return create a publicMethod property that is a function that
-invokes privateMethod. After you create the privateMethod. Invoke it by calling
-module.publicMethod(); outside the module scope */
+ invokes privateMethod. After you create the privateMethod. Invoke it by calling
+ module.publicMethod(); outside the module scope */
 
-var module = (function() {
-  var person = {
-    name: "phillip",
-    age: 29,
-    location: 'Utah'
-  };
+var module = (function ()
+{
+    var person = {
+        name: "phillip",
+        age: 29,
+        location: 'Utah'
+    };
 
-  var privateMethod = function(){
-    return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
-  };
+    var privateMethod = function ()
+    {
+        return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
+    };
 
-  // Anything that is being returned is made public and can be invoked from
-	// outside our lexical scope
+    // Anything that is being returned is made public and can be invoked from
+    // outside our lexical scope
 
-  return {
-    // Code here.
-  };
+    return {
+        // Code here.
+        publicMethod: function ()
+        {
+            return privateMethod();
+        }
+    };
 
 })();
 
 // Uncomment this after you create your public method
-//   module.publicMethod();
-
-
-
-
-
-
-
-
+console.log(module.publicMethod());
 
 
 /******************************************************************************\
-	#PROBLEM-07
-\******************************************************************************/
+ #PROBLEM-07
+ \******************************************************************************/
 
 /****** INSTRUCTIONS PROBLEM 7 ******/
 /* Here we have a for loop that will iterate as long as i is less than or equal
-to 5. What we need to do is console.log(i) so that it logs ( 0 then 1 then 2
-then 3, etc). Run this code in your console to see what the output is. */
+ to 5. What we need to do is console.log(i) so that it logs ( 0 then 1 then 2
+ then 3, etc). Run this code in your console to see what the output is. */
 
 // To make this code work you will need to create a new scope for every iteration.
-function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000)
-  }
+function timeOutCounter()
+{
+    for (var i = 0; i <= 5; i++)
+    {
+        setTimeout(logit(i), i * 1000)
+    }
 
-  function newScope(i) {
-    console.log(i)
-  }
+    function logit(num)
+    {
+        return function newScope()
+        {
+            console.log(num);
+        };
+    }
 }
 timeOutCounter();
 
 
-
-
-
-
 /******************************************************************************\
-	#PROBLEM-08
-\******************************************************************************/
+ #PROBLEM-08
+ \******************************************************************************/
 
 var funcArray = [];
 
-/*
-  Make the following code work
+function insertFunction(num)
+{
+    funcArray.push(function ()
+    {
+        return num;
+    })
+}
 
-  funcArray[0]() //0
-  funcArray[1]() //1
-  funcArray[2]() //2
-  funcArray[3]() //3
-  funcArray[4]() //4
-  funcArray[5]() //5
+for(var i = 0; i < 6; i++)
+{
+    insertFunction(i);
+}
 
-  *Hint: Don't let this fool you. Break down what's really happening here.
-*/
+// Make the following code work
+
+ console.log("funcArray:", funcArray[0]()); //0
+ console.log("funcArray:", funcArray[1]()); //1
+ console.log("funcArray:", funcArray[2]()); //2
+ console.log("funcArray:", funcArray[3]()); //3
+ console.log("funcArray:", funcArray[4]()); //4
+ console.log("funcArray:", funcArray[5]()); //5
+
+ //Hint: Don't let this fool you. Break down what's really happening here.
+
